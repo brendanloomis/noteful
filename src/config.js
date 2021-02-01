2c21450996640754ed89667890b959a5de5eacd6
Brendan Loomis <brendanloomis@gmail.com>
2021-01-31 16:50:15 -0800

    fix api url again

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://guarded-thicket-17063.herokuapp.com/',
+    API_ENDPOINT: 'https://guarded-thicket-17063.herokuapp.com',
     API_KEY: process.env.REACT_APP_API_KEY
 };
